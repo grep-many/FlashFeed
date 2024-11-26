cf62aaecc6d1c34c8af5e9b5cc45185a3d4dd5b0
manish <me.manishkumaryadav@gmail.com>
2024-11-27 01:59:55 +0530

    updated Index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get('/news', async (req, res) => {
     const { country, category, page, pageSize } = req.query;
